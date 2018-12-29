c069c9a28a9ff8b07ac7d546825a13b6c78a19f5
Simon Kong <simon.kong95@gmail.com>
2018-12-29 23:11:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,13 @@
+require "pry"
+
 def consolidate_cart(cart)
   # code here
+  item = {}
+  cart.each do |grocery, group_data|
+    group_data.each do |name, data|
+      binding.pry
+    end
+  end
 end
 
 def apply_coupons(cart, coupons)
@@ -13,3 +21,18 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+list = [
+  {"AVOCADO" => {:price => 3.00, :clearance => true}},
+  {"KALE" => {:price => 3.00, :clearance => false}},
+  {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+  {"ALMONDS" => {:price => 9.00, :clearance => false}},
+  {"TEMPEH" => {:price => 3.00, :clearance => true}},
+  {"CHEESE" => {:price => 6.50, :clearance => false}},
+  {"BEER" => {:price => 13.00, :clearance => false}},
+  {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+  {"BEETS" => {:price => 2.50, :clearance => false}},
+  {"SOY MILK" => {:price => 4.50, :clearance => true}}
+]
+
+consolidate_cart(list)
