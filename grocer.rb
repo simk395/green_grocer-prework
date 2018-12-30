2f54a06b51f4569757495020e6f9142cd102981a
Simon Kong <simon.kong95@gmail.com>
2018-12-30 02:34:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,5 +53,9 @@ def checkout(cart, coupons)
   cart = consolidate_cart(cart)
   cart = apply_coupons(cart, coupons)
   cart = apply_clearance(cart)
+  total = 0
+  cart.each do |item, data|
+   total += data[:price]
+  end
 
 end
