7f54e166d8bbf061912a2051bbbeae378d1e2d6c
Simon Kong <simon.kong95@gmail.com>
2018-12-30 00:09:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,7 +29,12 @@ def apply_coupons(cart, coupons)
   end
 end
 
-[
+a = {
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 2},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
+}
+
+b = [
   {:item => "AVOCADO", :num => 2, :cost => 5.00},
   {:item => "BEER", :num => 2, :cost => 20.00},
   {:item => "CHEESE", :num => 3, :cost => 15.00}
